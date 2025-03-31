fb89baf96f6116f407d8090703e7fd2019bc99a5
cjwei <changjia@umich.edu>
2025-03-31 17:55:03 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+
+
+// require our new middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+
+const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
+const usersController = require('./controllers/users.js')
+
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passUserToView);
+
+app.get('/', (req, res) => {
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/foods`);
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render('home.ejs');
+  }
+});
+
+app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods', foodsController);
+app.use('/userslist', usersController)
+
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
